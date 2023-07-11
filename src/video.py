148fb2a644c052c2c93ead5b4f6976dc4798760d
Hp <40killer40@mail.ru>
2023-07-11 23:15:56 +0300

    правки по 4-му дз

--- src/video.py
+++ src/video.py
@@ -2,13 +2,14 @@ import json
 import os
 from googleapiclient.discovery import build
 
+
 class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
 
         video_info = Video.get_service().videos().list(id=video_id, part='snippet,statistics').execute()
-        json_format = json.loads(json.dumps((video_info)))
+        json_format = json.loads(json.dumps(video_info))
 
         self.video_title = json_format['items'][0]['snippet']['title']
         self.video_url = f'https://www.youtube.com/watch?v={self.video_id}'
@@ -22,7 +23,6 @@ class Video:
     def get_service(cls):
         api_key: str = os.getenv('YT_API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
-
 
 
 class PLVideo(Video):
